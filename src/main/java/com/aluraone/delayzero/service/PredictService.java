4bb1e2ec74b0b4038e6beaaad9425e2739c8b831
Remi <demiank9001@gmail.com>
2026-01-04 17:04:02 -0300

    cambio de version a Spring Boot 3 para compatibilidad con openapi-swagger

--- src/main/java/com/aluraone/delayzero/service/PredictService.java
+++ src/main/java/com/aluraone/delayzero/service/PredictService.java
@@ -1,15 +1,12 @@
 package com.aluraone.delayzero.service;
 
 
-import ai.onnxruntime.OrtEnvironment;
-import ai.onnxruntime.OrtException;
 import ai.onnxruntime.OrtSession;
+import com.aluraone.delayzero.dto.in.PredictionRequest;
+import com.aluraone.delayzero.service.ml.FeatureBuilder;
 import com.aluraone.delayzero.service.ml.ModelLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class PredictService {
@@ -19,7 +16,11 @@ public class PredictService {
 
     private OrtSession session;
 
-    public void callModel(){
+    public void getPrediction(PredictionRequest flightData){
+
+        var builder = new FeatureBuilder(flightData);
+
+
         session = loader.loadModel();
     }
 }
--- src/main/java/com/aluraone/delayzero/service/ml/FeatureBuilder.java
+++ src/main/java/com/aluraone/delayzero/service/ml/FeatureBuilder.java
@@ -1,4 +1,13 @@
 package com.aluraone.delayzero.service.ml;
 
+import com.aluraone.delayzero.dto.in.PredictionRequest;
+
 public class FeatureBuilder {
+
+    private PredictionRequest flightData;
+
+    public FeatureBuilder(PredictionRequest flightData){
+        this.flightData = flightData;
+    }
+
 }
